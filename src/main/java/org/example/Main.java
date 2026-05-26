1f8385e23cd99915c9a28ff704ee7245d5cb178d
Chathumal Sangeeth <38062287+cozyloon@users.noreply.github.com>
2026-05-26 17:00:45 +0530

    Add files via upload

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static Integer sum(int a, int b) {
+        int i = a + b;
+        return i;
+    }
+
+    public static Integer divide(int a, int b) {
+        int i = a / b;
+        return i;
+    }
+}
